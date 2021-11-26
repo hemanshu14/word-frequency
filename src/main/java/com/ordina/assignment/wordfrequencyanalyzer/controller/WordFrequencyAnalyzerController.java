ec3535a1f9b7e18f3efe1bdac414f914691973ce
Banga <hemanshu.banga@rabobank.nl>
2021-11-26 15:42:32 +0100

    Done changes to create API to find most frequent N words in a given sentence

--- src/main/java/com/ordina/assignment/wordfrequencyanalyzer/controller/WordFrequencyAnalyzerController.java
+++ src/main/java/com/ordina/assignment/wordfrequencyanalyzer/controller/WordFrequencyAnalyzerController.java
@@ -1,5 +1,6 @@
 package com.ordina.assignment.wordfrequencyanalyzer.controller;
 
+import com.ordina.assignment.wordfrequencyanalyzer.service.WordFrequency;
 import com.ordina.assignment.wordfrequencyanalyzer.service.WordFrequencyAnalyzer;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * WordFrequencyAnalyzerController
@@ -64,5 +66,24 @@ public class WordFrequencyAnalyzerController {
                     @ApiResponse(responseCode = "500", description = "Server Error", content = @Content(mediaType = "application/json"))})
     public int getWordFrequency(@Parameter(name = "text", required = true, description = "The text in which we need to find the frequency of the given word") @Valid @NonNull @PathVariable final String text, @Parameter(name = "word", required = true, description = "The word for which we need to find the frequency in the given text") @Valid @NonNull @PathVariable final String word) {
         return wordFrequencyAnalyzer.calculateFrequencyForWord(text, word);
+    }
+
+    /**
+     * Method to retrieve the most frequent N words in a given sentence
+     *
+     * @param text          required text in which we need to find the frequency of the given word
+     * @param numberOfWords required number of words for which the frequent words should be found in the text
+     * @return the integer value of the frequency of the given word in the given sentence
+     */
+    @GetMapping("/words/{text}/most/{numberOfWords}")
+    @ResponseBody
+    @Operation(summary = "Receive the frequency of the given word in a given sentence", //
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved the most frequent N words in a given sentence", //
+                            content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "500", description = "Server Error", content = @Content(mediaType = "application/json"))})
+    public List<WordFrequency> getMostFrequentNWords(@Parameter(name = "text", required = true, description = "The text in which we need to find the frequency of the given word") @Valid @NonNull @PathVariable final String text, @Parameter(name = "numberOfWords", required = true, description = "The number of words for which the frequent words should be found in the text") @Valid @NonNull @PathVariable final int numberOfWords) {
+        return wordFrequencyAnalyzer.calculateMostFrequentNWords(text, numberOfWords);
     }
 }
--- src/main/java/com/ordina/assignment/wordfrequencyanalyzer/service/impl/WordFrequencyAnalyzerImpl.java
+++ src/main/java/com/ordina/assignment/wordfrequencyanalyzer/service/impl/WordFrequencyAnalyzerImpl.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -26,10 +27,13 @@ import java.util.stream.Collectors;
 @Slf4j
 public class WordFrequencyAnalyzerImpl implements WordFrequencyAnalyzer {
 
+    private static final String STARTING_METHOD_REFERENCE = "Starting method {}";
+    private static final String ENDING_METHOD_REFERENCE = "Ending method {}";
+
     @Override
     public int calculateHighestFrequency(String text) {
         var methodName = "calculateHighestFrequency";
-        log.info("Starting method {}", methodName);
+        log.info(STARTING_METHOD_REFERENCE, methodName);
 
         var listOfWords = getListOfWords(text);
 
@@ -37,20 +41,20 @@ public class WordFrequencyAnalyzerImpl implements WordFrequencyAnalyzer {
             throw new DataNotFoundException("There are no alphabetical words present in the given text");
         }
 
-        Map<String, Long> result = listOfWords.stream().collect(
-                Collectors.groupingBy(Function.identity(),
+        var result = listOfWords.stream().collect(
+                Collectors.groupingBy(String::toLowerCase,
                         Collectors.counting()));
 
         var max = result.entrySet().stream().max(Map.Entry.comparingByValue()).orElseThrow();
 
-        log.info("Ending method {}", methodName);
+        log.info(ENDING_METHOD_REFERENCE, methodName);
         return max.getValue().intValue();
     }
 
     @Override
     public int calculateFrequencyForWord(String text, String word) {
         var methodName = "calculateFrequencyForWord";
-        log.info("Starting method {}", methodName);
+        log.info(STARTING_METHOD_REFERENCE, methodName);
 
         var listOfWords = getListOfWords(text);
 
@@ -63,23 +67,47 @@ public class WordFrequencyAnalyzerImpl implements WordFrequencyAnalyzer {
         if (frequency == 0) {
             throw new DataNotFoundException("There is no given word present in the given text");
         }
-        log.info("Ending method {}", methodName);
+        log.info(ENDING_METHOD_REFERENCE, methodName);
         return (int) frequency;
     }
 
     @Override
     public List<WordFrequency> calculateMostFrequentNWords(String text, int n) {
-        return null;
+        var methodName = "calculateMostFrequentNWords";
+        log.info(STARTING_METHOD_REFERENCE, methodName);
+
+        if (n == 0) {
+            throw new DataNotFoundException("Most frequent words should be found greater than 0");
+        }
+
+        var listOfWords = getListOfWords(text);
+        var wordFrequencyList = new ArrayList<WordFrequencyImpl>();
+
+        var result = listOfWords.stream().collect(
+                Collectors.groupingBy(Function.identity(),
+                        Collectors.counting()));
+
+        result.forEach((key, value) -> wordFrequencyList.add(new WordFrequencyImpl(key, value.intValue())));
+
+        log.info(ENDING_METHOD_REFERENCE, methodName);
+
+        return wordFrequencyList.stream().sorted(Comparator.comparing(WordFrequencyImpl::getFrequency).reversed()
+                .thenComparing(WordFrequencyImpl::getWord)).limit(n).collect(Collectors.toList());
     }
 
 
     private List<String> getListOfWords(String text) {
+        var methodName = "getListOfWords";
+        log.info(STARTING_METHOD_REFERENCE, methodName);
+
         var list = new ArrayList<String>();
         var regexPattern = Pattern.compile("[a-zA-Z]+");
         var regexMatch = regexPattern.matcher(text);
         while (regexMatch.find()) {
             list.add(regexMatch.group(0).toLowerCase());
         }
+
+        log.info(ENDING_METHOD_REFERENCE, methodName);
         return list;
     }
 }
